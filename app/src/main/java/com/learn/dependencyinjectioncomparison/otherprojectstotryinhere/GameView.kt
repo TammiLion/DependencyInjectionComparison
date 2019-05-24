bc0860f1db72e806407361b216defb27df032409
TammiLion <trp300@gmail.com>
2019-05-24 17:43:41 +0200

    Working on convenience classes for sprite animations.

--- app/src/main/java/com/learn/dependencyinjectioncomparison/otherprojectstotryinhere/GameView.kt
+++ app/src/main/java/com/learn/dependencyinjectioncomparison/otherprojectstotryinhere/GameView.kt
@@ -6,6 +6,9 @@ import android.view.MotionEvent
 import android.view.SurfaceView
 import com.learn.dependencyinjectioncomparison.R
 import android.util.TypedValue
+import com.learn.dependencyinjectioncomparison.otherprojectstotryinhere.kandroiddraw.SCALE_HALF
+import com.learn.dependencyinjectioncomparison.otherprojectstotryinhere.kandroiddraw.SCALE_X2
+import com.learn.dependencyinjectioncomparison.otherprojectstotryinhere.kandroiddraw.Sprite
 
 class GameView(context: Context, size: Point) : SurfaceView(context), Runnable {
 
@@ -22,6 +25,7 @@ class GameView(context: Context, size: Point) : SurfaceView(context), Runnable {
     private var roguelikeSheet: Bitmap = BitmapFactory.decodeResource(
             context.resources,
             R.drawable.roguelike_sheet)
+    private var rogueSprite = Sprite(context.resources, size, null, roguelikeSheet)
     private var currentInvadersFrame: Bitmap
     private var pastTime = 0.0f
     private val startTime = System.currentTimeMillis()
@@ -113,17 +117,22 @@ class GameView(context: Context, size: Point) : SurfaceView(context), Runnable {
             val dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                     16f, context.resources.displayMetrics)
 
-            
+            rogueSprite.scale = SCALE_HALF
+            rogueSprite.setSrc(Rect(0,0,32,64))
+            //rogueSprite.setDest(700,200,32,64)
+            rogueSprite.setDest(700,200)
+            rogueSprite.draw(canvas)
+
             val src = Rect(0, 0, (dp*2).toInt(), (dp*4).toInt())
             val dest = Rect(800, 200, (800+(dp*2)).toInt(), (200+(dp*4)).toInt())
             canvas.drawRect(dest, paint)
             canvas.drawBitmap(roguelikeSheet, src, dest, null)
 
-            val src2 = Rect(64, 0, 128, 64)
+            /*val src2 = Rect(64, 0, 128, 64)
             val dest2 = Rect(200, 200, 264, 264)
             canvas.drawRect(src2, paint)
             canvas.drawRect(dest2, paint)
-            canvas.drawBitmap(roguelikeSheet, src2, dest2, null)
+            canvas.drawBitmap(roguelikeSheet, src2, dest2, null)*/
 
 
             // Draw the score and remaining lives
---
+++ app/src/main/java/com/learn/dependencyinjectioncomparison/otherprojectstotryinhere/kandroiddraw/Sprite.kt
@@ -0,0 +1,52 @@
+package com.learn.dependencyinjectioncomparison.otherprojectstotryinhere.kandroiddraw
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.*
+import android.util.TypedValue
+
+const val DEFAULT_SCALE = 1f
+const val SCALE_X2 = 2f
+const val SCALE_X4 = 4f
+const val SCALE_HALF = 0.5f
+
+class Sprite(resources: Resources, size: Point, drawableRes: Int?, private var bitmap: Bitmap?) {
+
+    private var src = Rect(0,0,0,0)
+    private var dest = Rect(0,0,0,0)
+    var paint: Paint? = null
+    var scale: Float = DEFAULT_SCALE
+    private val pixToDp: Float = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            1f, resources.displayMetrics)
+
+    init {
+        if(bitmap == null && drawableRes != null) {
+            bitmap = BitmapFactory.decodeResource(resources, drawableRes)
+        }
+    }
+
+    fun setSrc(src: Rect, adaptToScreenDensity: Boolean = true) {
+        this.src = if(adaptToScreenDensity) getRectangleAdaptedToDensity(src) else src
+    }
+
+    fun setDest(x: Int, y: Int, width: Int? = null, height: Int? = null, adaptToScreenDensity: Boolean = true) {
+        dest = if(width == null || height == null) {
+             Rect(x, y, (x+(src.width()*scale)).toInt(), (y+(src.height()*scale)).toInt())
+        } else {
+            if(adaptToScreenDensity) {
+                Rect(x, y, x+(width*pixToDp*scale).toInt(), y+(height*pixToDp*scale).toInt())
+            } else {
+                Rect(x, y, (x+(width*scale)).toInt(), (y+(height*scale)).toInt())
+            }
+        }
+    }
+
+    private fun getRectangleAdaptedToDensity(rect: Rect): Rect {
+        return Rect((rect.left*pixToDp).toInt(), (rect.top*pixToDp).toInt(), (rect.right*pixToDp).toInt(), (rect.bottom*pixToDp).toInt())
+    }
+
+    fun draw(canvas: Canvas) {
+        bitmap?.let { canvas.drawBitmap(it, src, dest, paint) }
+    }
+
+}
